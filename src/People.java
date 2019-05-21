4f4c455f16c12fe13372aea1ffaa96f6a1aecaf0
TrollSpace <miger2000@mail.ru>
2019-05-22 01:13:34 +0300

    Научил человека бегать и прыгать.

---
+++ src/People.java
@@ -0,0 +1,9 @@
+public class People {
+    public void run(){
+        System.out.println("Человек побежал");
+    }
+
+    public void jump(){
+        System.out.println("Человек прыгнул");
+    }
+}
