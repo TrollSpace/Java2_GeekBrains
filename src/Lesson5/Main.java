113c42456667c30e8233ba22e332ad0d63f32942
TrollSpace <miger2000@mail.ru>
2019-06-08 17:09:44 +0300

    change fill array by one. Add callculate time for one thread.

--- src/Lesson5/Main.java
+++ src/Lesson5/Main.java
@@ -1,14 +1,26 @@
 package Lesson5;
 
+import java.util.Arrays;
+
 public class Main {
     static final int size = 10000000;
     static final int h = size / 2;
     static float[] arr = new float[size];
 
-    public static void fillArrayByOne() {
-        for (int x = 0; x<arr.length; x++) {
-            arr[x] = 1;
-        }
+    public static void main(String[] args) {
+        oneThreadArray();
+    }
 
+    public static void fillArrayByOne() {
+        Arrays.fill(arr, 1);
+    }
+
+    public static void oneThreadArray() {
+        fillArrayByOne();
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis()-a);
     }
 }
