686fe309bf9ea38adb4761370d959df8d5d41d57
TrollSpace <miger2000@mail.ru>
2019-06-16 23:31:09 +0300

    Add input and output.

--- src/Lesson6/Server/ChatServer.java
+++ src/Lesson6/Server/ChatServer.java
@@ -1,8 +1,12 @@
 package Lesson6.Server;
 
 
-import java.io.*;
-import java.net.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class ChatServer {
     private ServerSocket serversocket;
@@ -16,6 +20,23 @@ public class ChatServer {
             System.out.println("Client connected with IP: " + socket.getInetAddress().getHostAddress());
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            Scanner sc = new Scanner(System.in);
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String msg = sc.nextLine();
+                        out.writeUTF(msg);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            while(true){
+                String msg = in.readUTF();
+                System.out.println(msg);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
